ee0ddbd8f1c1682c74d357796ae89d6224361e88
Redsiana <redsiana@orange.fr>
2018-01-19 11:26:01 +0100

    changed timeline ([D-C-M-S]*tps -save)

--- master.R
+++ master.R
@@ -33,11 +33,20 @@ master <- function( s,
   
   Ninit <- K * fec
   
+
+  
+  # preparing data gathering
+  
+  INVASION_tot <- numeric( length = tps ) # pop size gathered once adults have settled and survived competition, before reproduction
+  INVASION_asex <- numeric( length = tps )
+  EDGE_t <- numeric( length = tps ) # position of the pop edge in time (defined as the X coordinates where all Y patches contain individuals)
+  
+  
   # Initialization
   
   # input: K, Ninit, G
   # output: popgenome, sex, newbabyX, newbabyY
-   
+  
   TEMP <- genesis( K = K, fec = fec, Xinit, Yinit, Ninit = Ninit, G = G) 
   
   popgenome <- TEMP$popgenome
@@ -48,32 +57,16 @@ master <- function( s,
   popcloneline <- TEMP$popcloneline
   popclonalorigin <- TEMP$popclonalorigin
   
-  # 0. Initial dispersal
-  # 
-  # input: newbabyX, newbabyY, 
-  # output: newpopX, newpopY, newpopXY, coordalive, haspartner
+
   
-  TEMP <- diaspora( newbabyX = newbabyX, newbabyY = newbabyY, 
-                    dispkernel = dispkernel, sex = sex, repro = repro,
-                    Xdim = Xdim, Ydim = Ydim)
+  #   # saves the reproductive population size per patch, after the first ever round of dispersion
+  #   popsize_past <- tapply( !is.na(coordalive), coordalive, sum )
+  #   popXY_past <- popXY
+  #   popX_past <- popX
+  #   popY_past <- popY
   
-  popXY <- TEMP$newpopXY
-  popY <- TEMP$newpopY
-  popX <- TEMP$newpopX
-  coordalive <- TEMP$coordalive
-  haspartner <- TEMP$haspartner
   
-#   # saves the reproductive population size per patch, after the first ever round of dispersion
-#   popsize_past <- tapply( !is.na(coordalive), coordalive, sum )
-#   popXY_past <- popXY
-#   popX_past <- popX
-#   popY_past <- popY
   
-  # preparing data gathering
-  
-  INVASION_tot <- numeric( length = tps ) # pop size gathered once adults have settled and survived competition, before reproduction
-  INVASION_asex <- numeric( length = tps )
-  EDGE_t <- numeric( length = tps ) # position of the pop edge in time (defined as the X coordinates where all Y patches contain individuals)
   
   # number of asexuals ever to arise through mutation
   count <- 0
@@ -82,6 +75,21 @@ master <- function( s,
   
   for ( t in 1: tps ){
     
+    # 0. Initial dispersal
+    # 
+    # input: newbabyX, newbabyY, 
+    # output: newpopX, newpopY, newpopXY, coordalive, haspartner
+    
+    TEMP <- diaspora( newbabyX = newbabyX, newbabyY = newbabyY, 
+                      dispkernel = dispkernel, sex = sex, repro = repro,
+                      Xdim = Xdim, Ydim = Ydim)
+    
+    popXY <- TEMP$newpopXY
+    popY <- TEMP$newpopY
+    popX <- TEMP$newpopX
+    coordalive <- TEMP$coordalive
+    haspartner <- TEMP$haspartner
+    
     
     # 1. Competition
     # 
@@ -89,7 +97,8 @@ master <- function( s,
     # output: popsurvival
     
     ## to switch on genotype competition
-    popsurvival <- competition( haspartner = haspartner, 
+    popsurvival <- competition( compet = compet,
+                                haspartner = haspartner, 
                                 coordalive = coordalive, 
                                 popgenome = popgenome, K = K, G = G)
     
@@ -170,38 +179,21 @@ master <- function( s,
     # ani.record()
     # 
     
-      source("D:/Kokkonuts/Where sex/modelling/nouvelle geographie/analysis_cloneposition.R", local = TRUE)
+      source("analysis_cloneposition.R", local = TRUE)
     
     }
     
     
-    
-    
-    
-    # 5. Dispersal - voyage
-    # 
-    # input: newbabyX, newbabyY, dispersal kernel, sex, repro
-    # output: newpopX, newpopY, newpopXY, coordalive, haspartner
-    
-    # TEMP <- diaspora( newbabyX = newbabyX, newbabyY = newbabyY, short_dist_sd = short_dist_sd, plongdist = plongdist, long_dist_sd = long_dist_sd, long_dist_mean = long_dist_mean)
-    
-    TEMP <- diaspora( newbabyX = newbabyX, newbabyY = newbabyY, 
-                      dispkernel = dispkernel, sex = sex, repro = repro,
-                      Xdim = Xdim, Ydim = Ydim)
-    
-    popXY <- TEMP$newpopXY
-    popY <- TEMP$newpopY
-    popX <- TEMP$newpopX
-    coordalive <- TEMP$coordalive
-    haspartner <- TEMP$haspartner
+
     
     # Limit cases
     if( length( popXY ) == 0 | sum ( is.na(coordalive) ) == length(coordalive) ){
       popsurvival <- NA
       break
     } 
-    if( sum( repro == "s" ) / sum( repro == "a") < 0.1 ){
-      popsurvival <- competition( haspartner = haspartner, 
+    if( sum( repro == "s" ) / sum( repro == "a") < 0.05 ){
+      popsurvival <- competition( compet = compet, 
+                                  haspartner = haspartner, 
                                   coordalive = coordalive, popgenome = popgenome,
                                   K = K, G = G)
       break
@@ -211,14 +203,11 @@ master <- function( s,
     
     print( paste( run, ':', t ) )
     
+    
+    
+    
   }
   
-  if( t == tps ) popsurvival <- competition( haspartner = haspartner, 
-                                             coordalive = coordalive, 
-                                             popgenome = popgenome, K = K, G = G)
-  
-
-
   
   
   
@@ -235,59 +224,7 @@ master <- function( s,
  
  
  
- 
- 
- 
- 
- sorted_clone_prevalence <- sort( table(popcloneline)[-1])# gives table with clone number and number of individuals
- main_clones_names <- as.numeric( names( tail(sorted_clone_prevalence, 5) ))
- 
- clone_bars <- as.numeric( table(popX[ !(popcloneline %in% c(0,main_clones_names)) ]) )
- clone_patches <- as.numeric( names( table(popX[ !(popcloneline %in% c(0,main_clones_names)) ])) )
- clone_idcol <- as.factor( rep( "other cl", length(clone_bars)) )
- df <- data.frame(x = clone_patches, y = clone_bars, fac = clone_idcol )
- 
- x_emergence <- numeric(length = length(main_clones_names))
- g_emergence <- numeric(length = length(main_clones_names))
- g_edge <- numeric(length = length(main_clones_names))
- label_clone <- numeric(length = length(main_clones_names))
- 
- for(clone in 1:length(main_clones_names)){
-   clone_bars <- as.numeric( table(popX[ popcloneline == main_clones_names[clone]]) )
-   clone_patches <- as.numeric( names( table(popX[ popcloneline == main_clones_names[clone]])) )
-   clone_idcol <- as.factor( rep( main_clones_names[clone], length(clone_bars)) )
-   df <- rbind(df, data.frame(x = clone_patches, y = clone_bars, fac = clone_idcol ))
-   
-   x_emergence[clone] <- unique( popclonalorigin[popcloneline %in% main_clones_names[clone],1] )
-   g_emergence[clone] <- unique( popclonalorigin[popcloneline %in% main_clones_names[clone],2] )
-   g_edge[clone] <- EDGE_t[g_emergence[clone]]
-   label_clone[clone] <- paste(g_emergence[clone], ":", x_emergence[clone], '-', g_edge[clone])
- }
- 
- sexual_bars <- as.numeric( table(popX[ popcloneline == 0]) )
- sexual_patches <- as.numeric( names( table(popX[ popcloneline == 0])) )
- sexual_idcol <- rep("sex", length(sexual_bars))
- df <- rbind( df, data.frame(x = sexual_patches, y = sexual_bars, fac = sexual_idcol ) )
- 
- 
- 
- # this df has 3 columns, the X-coordinates, the number of individuals, the identity (5 main clones, other cl or sex)
- mypalette <- c( brewer.pal((length(main_clones_names)+1),  "YlOrRd"), "black" )
- 
- labels_plot <- c("other cl", label_clone, "sex")
- 
- ggplot(data=df, aes(x=x, y=y, fill=fac, order=fac)) +
-   geom_bar(stat="identity" ) +   theme_minimal() +
-   scale_fill_manual(values= mypalette, labels = labels_plot) +
-   geom_vline(xintercept = jitter(x_emergence), col=mypalette[2:(length(main_clones_names)+1)], size = .8 )+
-   ggtitle(paste("pop composition after", t, "generations")) +
-   theme(aspect.ratio=5/10)
- 
- # this prints, for each of the 5 main clones, the generation at which it appeared, the x coordinates, and the coordinates of the edge back then
- 
- ggsave(filename = paste(namerun, ".jpg", sep=""))
- 
- 
+ source("analysis_cloneposition.R")
  
 
  
