bbb28901b397aa297b6adeb6e7da7e3d599a93ca
Redsiana <redsiana@orange.fr>
2018-09-11 16:54:35 +0200

    diversity index shannon is now per whole genotype

--- with htz data collection/shannon_n_htz.R
+++ with htz data collection/shannon_n_htz.R
@@ -8,27 +8,45 @@
     # how much was eaten of each category of the 2*G. Pop of total heteroz would consume 1/2G of each.
     newbabyXY = paste(newbabyX, ",", newbabyY, sep="")
     occupiedpatches <- sort( unique( newbabyXY ) )
-    patch_compet <- matrix( nrow = length(occupiedpatches) , ncol= 2*G )
-    rownames( patch_compet ) = occupiedpatches
-    for (g in 1:G){
-      Tgenepatch <- table( factor( popgenome[,g], levels = 0:2), newbabyXY )
-      #indivperpatch <- colSums( Tgenepatch )
-      patch_compet[,(g*2-1)] <- Tgenepatch[1,]*2 + Tgenepatch[2,] # vector with number of 0 alleles on the patch
-      patch_compet[,(g*2)] <- Tgenepatch[3,]*2 + Tgenepatch[2,] # vector with number of 1 alleles on the patch
-    }
-    freq_sp <- ( patch_compet / (apply(patch_compet, 1, sum) /G ) )
-    log_freq_sp <- log( freq_sp )
-    log_freq_sp[ is.infinite( log_freq_sp ) ] <- 0
-    shannon <- - apply( freq_sp * log_freq_sp, 1, sum ) / G
-    std_shannon <- shannon / ( - log(0.5))
-    
     X_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(TRUE,FALSE) ] )
-    Y_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(FALSE, TRUE) ] )
+    # patch_compet <- matrix( nrow = length(occupiedpatches) , ncol= 3*G )
+    # rownames( patch_compet ) = occupiedpatches
+    # for (g in 1:G){
+    #   Tgenepatch <- table( factor( popgenome[,g], levels = 0:2), newbabyXY )
+    #   #indivperpatch <- colSums( Tgenepatch )
+    #   patch_compet[,(g*3-2)] <- Tgenepatch[1,] # vector with number of 00 genotypes on the patch
+    #   patch_compet[,(g*3-1)] <- Tgenepatch[2,] # vector with number of 01 genotypes on the patch
+    #   patch_compet[,(g*3)] <- Tgenepatch[3,]
+    # }
+    # freq_sp <- ( patch_compet / (apply(patch_compet, 1, sum)/G) ) 
+    # log_freq_sp <- log2( freq_sp )
+    # log_freq_sp[ is.infinite( log_freq_sp ) ] <- 0
+    # 
+    # shannon <- - apply( freq_sp * log_freq_sp, 1, sum )
+    # std_shannon <- shannon / ( - log2 (3) )
+    # 
+    # X_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(TRUE,FALSE) ] )
+    # Y_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(FALSE, TRUE) ] )
+    # 
+    # shannon_gradient <- tapply(std_shannon, X_position, mean) 
+    # res_shannon <- rep(NA, Xdim)
+    # res_shannon[ as.numeric( names(shannon_gradient)) + 1] <- shannon_gradient
+    # 
+    # 
     
-    shannon_gradient <- tapply(std_shannon, X_position, mean) 
+    shannon_function = function(x) {
+      ## BEWARE this is not a true Shannon Index. The normalizing factor is if
+      ## all individuals on the patch were different, which is not always 
+      ## evn possible if G is low
+      ( - sum( ( table(x) / length(x) ) * log2 ( table(x) / length(x) ) ) ) / log2( length(x) )
+    }
+    
+    genome <- apply(popgenome, 1, paste, collapse="")
+    shannon_per_patch <- tapply( genome, newbabyXY, shannon_function )
+    shannon_per_X <- tapply( shannon_per_patch, X_position, mean )
     res_shannon <- rep(NA, Xdim)
-    res_shannon[ as.numeric( names(shannon_gradient)) + 1] <- shannon_gradient
-    
+    res_shannon[ as.numeric( names(shannon_per_X)) + 1] <- shannon_per_X
+
     return(res_shannon)
   }
   
