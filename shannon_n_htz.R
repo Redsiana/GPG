c71b6805fa660d64bad8530a7edcee944f9a8195
Anaïs Tilquin <redsiana@orange.fr>
2018-10-10 16:37:52 +0200

    Add files via upload
    change normalization of shannon index - now takes into account number of indiv on patch, and number of possible genotypes given G

--- shannon_n_htz.R
+++ shannon_n_htz.R
@@ -2,33 +2,50 @@
   analysis_shannon <- function( newbabyX = newbabyX,
                     newbabyY = newbabyY,
                     popgenome = popgenome,
-                    G = G){
+                    G = G, Xdim = Xdim ){
     #### PHENOTYPIC DIVERSITY -> Shannon index based on RESOURCE CONSUMPTION
     # some kind of shannon index based on the resource consumed. Of the total resource consumed, 
     # how much was eaten of each category of the 2*G. Pop of total heteroz would consume 1/2G of each.
     newbabyXY = paste(newbabyX, ",", newbabyY, sep="")
     occupiedpatches <- sort( unique( newbabyXY ) )
-    patch_compet <- matrix( nrow = length(occupiedpatches) , ncol= 2*G )
-    rownames( patch_compet ) = occupiedpatches
-    for (g in 1:G){
-      Tgenepatch <- table( factor( popgenome[,g], levels = 0:2), newbabyXY )
-      #indivperpatch <- colSums( Tgenepatch )
-      patch_compet[,(g*2-1)] <- Tgenepatch[1,]*2 + Tgenepatch[2,] # vector with number of 0 alleles on the patch
-      patch_compet[,(g*2)] <- Tgenepatch[3,]*2 + Tgenepatch[2,] # vector with number of 1 alleles on the patch
-    }
-    freq_sp <- ( patch_compet / (apply(patch_compet, 1, sum) /G ) )
-    log_freq_sp <- log( freq_sp )
-    log_freq_sp[ is.infinite( log_freq_sp ) ] <- 0
-    shannon <- - apply( freq_sp * log_freq_sp, 1, sum ) / G
-    std_shannon <- shannon / ( - log(0.5))
-    
     X_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(TRUE,FALSE) ] )
-    Y_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(FALSE, TRUE) ] )
+    # patch_compet <- matrix( nrow = length(occupiedpatches) , ncol= 3*G )
+    # rownames( patch_compet ) = occupiedpatches
+    # for (g in 1:G){
+    #   Tgenepatch <- table( factor( popgenome[,g], levels = 0:2), newbabyXY )
+    #   #indivperpatch <- colSums( Tgenepatch )
+    #   patch_compet[,(g*3-2)] <- Tgenepatch[1,] # vector with number of 00 genotypes on the patch
+    #   patch_compet[,(g*3-1)] <- Tgenepatch[2,] # vector with number of 01 genotypes on the patch
+    #   patch_compet[,(g*3)] <- Tgenepatch[3,]
+    # }
+    # freq_sp <- ( patch_compet / (apply(patch_compet, 1, sum)/G) ) 
+    # log_freq_sp <- log2( freq_sp )
+    # log_freq_sp[ is.infinite( log_freq_sp ) ] <- 0
+    # 
+    # shannon <- - apply( freq_sp * log_freq_sp, 1, sum )
+    # std_shannon <- shannon / ( - log2 (3) )
+    # 
+    # X_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(TRUE,FALSE) ] )
+    # Y_position <- as.numeric( unlist( strsplit(occupiedpatches, ",") )[ c(FALSE, TRUE) ] )
+    # 
+    # shannon_gradient <- tapply(std_shannon, X_position, mean) 
+    # res_shannon <- rep(NA, Xdim)
+    # res_shannon[ as.numeric( names(shannon_gradient)) + 1] <- shannon_gradient
+    # 
+    # 
     
-    shannon_gradient <- tapply(std_shannon, X_position, mean) 
+    shannon_function = function(x) {
+      ## BEWARE this is not a true Shannon Index. The normalizing factor is if
+      ## all individuals on the patch were different, or if all possible genotypes are represented
+      ( - sum( ( table(x) / length(x) ) * log2 ( table(x) / length(x) ) ) ) / min( log2( length(x) ), log2( 3^G ) )
+    }
+    
+    genome <- apply(popgenome, 1, paste, collapse="") # collapses loci in 1 string
+    shannon_per_patch <- tapply( genome, newbabyXY, shannon_function )
+    shannon_per_X <- tapply( shannon_per_patch, X_position, mean )
     res_shannon <- rep(NA, Xdim)
-    res_shannon[ as.numeric( names(shannon_gradient)) + 1] <- shannon_gradient
-    
+    res_shannon[ as.numeric( names(shannon_per_X)) + 1] <- shannon_per_X
+
     return(res_shannon)
   }
   
@@ -40,7 +57,7 @@
 
   analysis_htz <- function( popgenome = popgenome,
                             G = G,
-                            newbabyX = newbabyX){
+                            newbabyX = newbabyX, Xdim = Xdim){
     # Ahtz <- rowSums( popgenome[ popcloneline == 0, ] ==1 )/G
     # Shtz <- rowSums( popgenome[ popcloneline != 0, ] ==1 )/G
     Thtz <- rowSums( popgenome ==1 )/G
